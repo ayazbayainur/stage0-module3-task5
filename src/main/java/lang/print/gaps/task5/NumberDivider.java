d1924174c00116fd76a75fc154215cd7d2e49c6f
Ainur <ayazbay.ainur@mail.ru>
2023-07-07 22:35:29 +0600

    1st commit

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        System.out.println((double) toBeDivided / 5);
+    }
+
+    public static void main(String[] args){
+        var classInstance = new NumberDivider();
+        classInstance.divide(19);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds = minutes * 60;
+        System.out.println(seconds);
+    }
+    public static void main(String[] args){
+        var classInstance = new TimeConvertor();
+        classInstance.convert(11.5f);
     }
 }
